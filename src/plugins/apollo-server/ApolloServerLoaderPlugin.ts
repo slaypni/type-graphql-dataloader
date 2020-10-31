92230385c26e6dc513f74ce560eb3d55fbcbcecf
Kazuaki Tanida <punigumi@gmail.com>
2020-10-31 10:42:51 +0900

    Fix: remove unecessary cast to ApolloServerPlugin

--- src/plugins/apollo-server/ApolloServerLoaderPlugin.ts
+++ src/plugins/apollo-server/ApolloServerLoaderPlugin.ts
@@ -1,4 +1,3 @@
-import { ApolloServerPlugin } from "apollo-server-plugin-base";
 import { Container } from "typedi";
 import { Connection } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
@@ -8,21 +7,20 @@ interface ApolloServerLoaderPluginOption {
   typeormGetConnection?: () => Connection;
 }
 
-const ApolloServerLoaderPlugin = (option?: ApolloServerLoaderPluginOption) =>
-  ({
-    requestDidStart: () => ({
-      didResolveSource(requestContext) {
-        Object.assign(requestContext.context, {
-          _tgdContext: {
-            requestId: uuidv4(),
-            typeormGetConnection: option?.typeormGetConnection,
-          } as TgdContext,
-        });
-      },
-      willSendResponse(requestContext) {
-        Container.reset(requestContext.context._tgdContext.requestId);
-      },
-    }),
-  } as ApolloServerPlugin);
+const ApolloServerLoaderPlugin = (option?: ApolloServerLoaderPluginOption) => ({
+  requestDidStart: () => ({
+    didResolveSource(requestContext: { context: Record<string, any> }) {
+      Object.assign(requestContext.context, {
+        _tgdContext: {
+          requestId: uuidv4(),
+          typeormGetConnection: option?.typeormGetConnection,
+        } as TgdContext,
+      });
+    },
+    willSendResponse(requestContext: { context: Record<string, any> }) {
+      Container.reset(requestContext.context._tgdContext.requestId);
+    },
+  }),
+});
 
 export { ApolloServerLoaderPlugin };
