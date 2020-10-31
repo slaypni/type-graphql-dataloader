dcc551bc1f3620918deb40c55fba849d05e50657
Kazuaki Tanida <punigumi@gmail.com>
2020-10-31 09:10:06 +0900

    Change not to pass root and info to batchLoadFn

--- src/decorators/Loader.ts
+++ src/decorators/Loader.ts
@@ -1,12 +1,17 @@
-import { UseMiddleware, ResolverData } from "type-graphql";
+import { UseMiddleware, ArgsDictionary } from "type-graphql";
 import { MethodAndPropDecorator } from "type-graphql/dist/decorators/types";
 import DataLoader from "dataloader";
 import Container from "typedi";
 import { TgdContext } from "#/types/TgdContext";
 
+interface ResolverData<ContextType = {}> {
+  args: ArgsDictionary;
+  context: ContextType;
+}
+
 type BatchLoadFn<K, V> = (
   keys: ReadonlyArray<K>,
-  data: ResolverData<any>
+  data: ResolverData
 ) => PromiseLike<ArrayLike<V | Error>>;
 
 export function Loader<K, V, C = K>(
@@ -18,16 +23,19 @@ export function Loader<K, V, C = K>(
     propertyKey: string | symbol,
     descriptor?: TypedPropertyDescriptor<any>
   ) => {
-    UseMiddleware(async (data, next) => {
+    UseMiddleware(async ({ args, context }, next) => {
       const serviceId = `tgd#${
         target.constructor.name
       }#${propertyKey.toString()}`;
-      const { requestId } = data.context._tgdContext as TgdContext;
+      const { requestId } = context._tgdContext as TgdContext;
       const container = Container.of(requestId);
       if (!container.has(serviceId)) {
         container.set(
           serviceId,
-          new DataLoader((keys) => batchLoadFn(keys, data), options)
+          new DataLoader(
+            (keys) => batchLoadFn(keys, { args, context }),
+            options
+          )
         );
       }
       const dataloader = container.get(serviceId);
