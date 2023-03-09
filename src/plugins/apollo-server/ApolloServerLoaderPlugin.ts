71c61a6ee503829e8ab5838dacc49afc95cf263d
geoyws <geoyws@gmail.com>
2023-03-09 16:49:07 +0800

    upgraded to support @apollo/server 4.4.1

--- src/plugins/apollo-server/ApolloServerLoaderPlugin.ts
+++ src/plugins/apollo-server/ApolloServerLoaderPlugin.ts
@@ -1,6 +1,5 @@
 import type { TgdContext } from "#/types/TgdContext";
-import type { ApolloServerPlugin } from "apollo-server-plugin-base";
-import type { BaseContext } from "apollo-server-types";
+import type { ApolloServerPlugin, BaseContext } from "@apollo/server";
 import { Container } from "typedi";
 import type { Connection } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
@@ -13,16 +12,18 @@ const ApolloServerLoaderPlugin = (
   option?: ApolloServerLoaderPluginOption
 ): ApolloServerPlugin => ({
   requestDidStart: async () => ({
-    async didResolveSource(requestContext: { context: BaseContext }) {
-      Object.assign(requestContext.context, {
+    async didResolveSource(requestContext: { contextValue: BaseContext }) {
+      Object.assign(requestContext.contextValue, {
         _tgdContext: {
           requestId: uuidv4(),
           typeormGetConnection: option?.typeormGetConnection,
         } as TgdContext,
       });
     },
-    async willSendResponse(requestContext: { context: BaseContext }) {
-      Container.reset(requestContext.context._tgdContext.requestId);
+    async willSendResponse(requestContext: {
+      contextValue: BaseContext & Record<string, any>;
+    }) {
+      Container.reset(requestContext.contextValue._tgdContext.requestId);
     },
   }),
 });
