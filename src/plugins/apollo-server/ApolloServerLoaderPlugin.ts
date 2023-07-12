3f15c7df4fba3068ff2078c65c6e0c2ae5bd7569
geoyws <geoyws@gmail.com>
2023-07-12 11:30:33 +0800

    will work with apollo server 3 or 4

--- src/plugins/apollo-server/ApolloServerLoaderPlugin.ts
+++ src/plugins/apollo-server/ApolloServerLoaderPlugin.ts
@@ -1,5 +1,10 @@
 import type { TgdContext } from "#/types/TgdContext";
-import type { ApolloServerPlugin } from "@apollo/server";
+import type {
+  ApolloServerPlugin,
+  BaseContext,
+  GraphQLRequestContextDidResolveSource,
+  GraphQLRequestContextWillSendResponse,
+} from "@apollo/server";
 import { Container } from "typedi";
 import type { Connection } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
@@ -8,28 +13,30 @@ interface ApolloServerLoaderPluginOption {
   typeormGetConnection?: () => Connection;
 }
 
+const getContext = (
+  requestContext:
+    | GraphQLRequestContextDidResolveSource<BaseContext>
+    | GraphQLRequestContextWillSendResponse<BaseContext>
+) =>
+  requestContext?.contextValue
+    ? requestContext.contextValue
+    : /* @ts-ignore */
+      requestContext.context;
+
 const ApolloServerLoaderPlugin = (
   option?: ApolloServerLoaderPluginOption
 ): ApolloServerPlugin => ({
   requestDidStart: async () => ({
-    async didResolveSource(requestContext: any) {
-      let targetProp = "context";
-      if (requestContext?.contextValue) {
-        targetProp = "contextValue";
-      }
-      Object.assign(requestContext[targetProp], {
+    async didResolveSource(requestContext) {
+      Object.assign(getContext(requestContext), {
         _tgdContext: {
           requestId: uuidv4(),
           typeormGetConnection: option?.typeormGetConnection,
         } as TgdContext,
       });
     },
-    async willSendResponse(requestContext: any) {
-      let targetProp = "context";
-      if (requestContext?.contextValue) {
-        targetProp = "contextValue";
-      }
-      Container.reset(requestContext[targetProp]._tgdContext.requestId);
+    async willSendResponse(requestContext) {
+      Container.reset(getContext(requestContext)._tgdContext.requestId);
     },
   }),
 });
